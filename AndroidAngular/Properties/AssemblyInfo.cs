42ae419cfbb4142e3369e65a35420dd65e896ba2
Magnus Sundström <magnus.s@comaround.se>
2016-04-06 15:01:10 +0200

    initial commit

---
+++ AndroidAngular/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AndroidAngular")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AndroidAngular")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b933cd82-63d2-40c9-8907-f295aa150486")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AndroidAngular/Scripts/angular/script.js
@@ -0,0 +1,38 @@
+﻿var appX = angular.module('mainApp', []);
+
+appX.controller('app', function ($scope) {
+    $scope.tasks = [];
+
+    var taskData = localStorage['tasksList'];
+
+    if (taskData !== undefined) {
+        $scope.tasks = JSON.parse(taskData);
+    }
+
+    $scope.searchEnter = function() {
+        if (event.which === 13 && $scope.task !== '') {
+            $scope.addTask();
+        }
+    };
+    $scope.addTask = function() {
+        $scope.tasks.push({'taskMessage': $scope.task, 'status':false });
+        $scope.task = '';
+        localStorage['tasksList'] = JSON.stringify($scope.tasks);
+    };
+    $scope.contentEdit = function (x) {
+        console.log(x);
+
+        for (i = 0; i < $scope.tasks.length; i++) {
+            if ($scope.tasks[i].taskMessage == x) {
+                $scope.tasks[i].taskMessage = event.target.innerText;
+            }
+        }
+
+        event.target.contentEditable = event.target.contentEditable == 'false' ? 'true' : 'false';
+    };
+    $scope.enterAgain = function(msg) {
+        if (event.which == 13 && msg != '') {
+            $scope.contentEdit(msg);
+        }
+    }
+});
